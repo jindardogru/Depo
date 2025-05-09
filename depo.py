4ab2b733e23d21e42f8bda94d809feffe76d64b6
jindardogru <150179672+jindardogru@users.noreply.github.com>
2025-05-09 11:30:56 +0300

    Update depo.py

--- depo.py
+++ depo.py
@@ -1,10 +1,4 @@
 {
-  "user1": {
-    "locked": false,
-    "password": "user1password"
-  },
-  "user2": {
-    "locked": true,
-    "password": "user2password"
-  }
+  "lock": false,
+  "password": "default_password"
 }
